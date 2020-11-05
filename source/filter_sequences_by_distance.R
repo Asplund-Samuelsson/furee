0d06d0b8242a9e44fab9cf400b711e8766f403a6
Johannes Asplund-Samuelsson <johannes.aspsam@gmail.com>
2020-11-05 14:52:28 +0100

    Added distance filtering WIP script

---
+++ source/filter_sequences_by_distance.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+
+# Define infiles
+levenshtein_file = "intermediate/train.standard_aa.LD.tab"
+goodlength_file = "intermediate/train.length_filtered.txt"
+
+# Load data
+levenshtein = read_tsv(levenshtein_file, col_names=c("SeqA", "SeqB", "LD"))
+goodlength = scan(goodlength_file, character())
+
+# Filter Levenshtein distances to length-filtered sequences
+levenshtein = filter(levenshtein, SeqA %in% goodlength, SeqB %in% goodlength)
+
+# Set distance threshold
+
+
+# Write filtered sequence IDs to outfile
+outfile = "intermediate/train.filtered.txt"
+write(unique(c(levenshtein$SeqA, levenshtein$SeqB)), outfile)
