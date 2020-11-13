f7e4af8143a00dbe3cec8fdf513847148958fb2b
Johannes Asplund-Samuelsson <johannes.aspsam@gmail.com>
2020-11-13 16:17:25 +0100

    Updated docker run commands with more mounts and Python argument usage

--- source/run_cpu_docker.sh
+++ source/run_cpu_docker.sh
@@ -1,1 +1,7 @@
-docker run -it -p 8888:8888 -p 6006:6006 -v `pwd`:/UniRep unirep-cpu:latest
+docker run -it -p 8888:8888 -p 6006:6006 \
+-v `pwd`/unirep/UniRep:/UniRep \
+-v `pwd`/data:/data \
+-v `pwd`/intermediate:/intermediate \
+-v `pwd`/results:/results \
+unirep-cpu:latest \
+$1
--- source/run_gpu_docker.sh
+++ source/run_gpu_docker.sh
@@ -1,1 +1,8 @@
-docker run --gpus all -it -p 8888:8888 -p 6006:6006 -v `pwd`:/UniRep unirep-gpu:latest
+docker run --gpus all -it -p 8888:8888 -p 6006:6006 \
+-v `pwd`/unirep/UniRep:/UniRep \
+-v `pwd`/data:/data \
+-v `pwd`/intermediate:/intermediate \
+-v `pwd`/results:/results \
+-v `pwd`/source:/source \
+unirep-gpu:latest \
+$1
