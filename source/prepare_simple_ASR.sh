38b06e21a6e32e2caf4592af40ede3503c649010
Johannes Asplund-Samuelsson <johannes.aspsam@gmail.com>
2020-11-19 11:58:48 +0100

    Revert to using unrooted tree for ASR

--- source/prepare_simple_ASR.sh
+++ source/prepare_simple_ASR.sh
@@ -20,44 +20,8 @@ intermediate/train.filtered.fasta \
 intermediate/train.cdhit_0.85.seqids.txt \
 intermediate/train.cdhit_0.85.Synechocystis.raw.fasta
 
-# Find closely related reference sequences
-phmmer --noali \
---tblout intermediate/Syn6803_P73922_FBPase.reference_hits.tblout.txt \
-data/Syn6803_P73922_FBPase.fasta data/kegg_uniprot_ids.FBPase.fasta
-
-# Extract hit IDs in order of best to worst hit
-grep -v "^#" intermediate/Syn6803_P73922_FBPase.reference_hits.tblout.txt \
-| cut -f 1 -d \  > intermediate/Syn6803_P73922_FBPase.reference_hits.txt
-
-# Get taxonomic information for reference hits
-(grep ">" data/kegg_uniprot_ids.FBPase.fasta | tr " " "\n" | \
-grep -P "^>|^OX=" | sed -e 's/^OX=//' | tr ">" "&" | tr "\n" "\t" | \
-tr "&" "\n" | sed -e 's/\t$//' | grep -v "^$") > \
-intermediate/kegg_uniprot_ids.FBPase.taxids_from_fasta.tab
-
-source/taxid-to-taxonomy.py \
--i intermediate/kegg_uniprot_ids.FBPase.taxids_from_fasta.tab \
--n data/ncbi/taxonomy/names.dmp \
--d data/ncbi/taxonomy/nodes.dmp \
--o results/kegg_uniprot_ids.FBPase.taxonomy.tab
-
-# Check that order is retained
-diff <(tail -n +2 results/kegg_uniprot_ids.FBPase.taxonomy.tab | cut -f 1,2) \
-<(cat intermediate/kegg_uniprot_ids.FBPase.taxids_from_fasta.tab)
-
-# Select the best non-Cyano hit that is not in the cluster
-source/select_best_ASR_outgroup.R
-# Generates "intermediate/best_outgroup_seqid.txt"
-
-# Extract outgroup sequence
-source/filter_fasta_by_id.py \
-data/kegg_uniprot_ids.FBPase.fasta \
-intermediate/best_outgroup_seqid.txt \
-intermediate/ASR_outgroup.fasta
-
-# Add outgroup and keep only ID in FASTA header in order to not mess up trees
-cat intermediate/train.cdhit_0.85.Synechocystis.raw.fasta \
-intermediate/ASR_outgroup.fasta | cut -f 1 -d \  \
+# Keep only ID in FASTA header in order to not mess up trees
+cat intermediate/train.cdhit_0.85.Synechocystis.raw.fasta | cut -f 1 -d \  \
 > intermediate/train.cdhit_0.85.Synechocystis.fasta
 
 # Align cluster sequences
@@ -67,10 +31,6 @@ mafft --thread 16 intermediate/train.cdhit_0.85.Synechocystis.fasta \
 # Make tree
 fasttreeMP intermediate/train.cdhit_0.85.Synechocystis.ali.fasta \
 > intermediate/train.cdhit_0.85.Synechocystis.tree
-
-# Re-root tree at outgroup
-source/reroot_ASR_input_tree.R
-# Generates "intermediate/train.cdhit_0.85.Synechocystis.rooted.tree"
 
 # Submit data to FireProt-ASR (http://loschmidt.chemi.muni.cz/fireprotasr/)
 # Use Synechocystis sequence as the "query"
